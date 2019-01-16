b4badab27b2c248fa430d70556e3ea4b1d264d4d
Po-Yu Kao <poyu_kao@ucsb.edu>
2019-01-16 13:06:02 -0800

    change file name

--- unet_ce_hard_per_im_c25_s4867.py
+++ unet_ce_hard_per_im_c25_s4867.py
@@ -8,11 +8,13 @@ model = 'unet_ce_hard_per_im_c25_s4867'
 
 if not os.path.exists(os.path.join(out_dir, model)): os.mkdir(os.path.join(out_dir, model))
 
-folds = ['fold0', 'fold1', 'fold2', 'fold3', 'fold4']
+#folds = ['fold0', 'fold1', 'fold2', 'fold3', 'fold4']
+
+folds = ['fold4']
 
 for fold in folds:
 	cfg_name = model+'_'+fold
 
 	out = os.path.join(out_dir, model)
 	
-	call(['python', 'train_unet.py', '--gpu', '2', '--cfg', cfg_name, '--out', out])
+	call(['python', 'train_unet.py', '--gpu', '1', '--cfg', cfg_name, '--out', out])
