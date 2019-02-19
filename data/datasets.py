96599210995dbe48bab6feca9aebcfe831a9999d
Po-Yu Kao <poyu_kao@ucsb.edu>
2019-02-18 19:15:00 -0800

    added DualData13 which is VOI

--- data/datasets.py
+++ data/datasets.py
@@ -1,2 +1,2 @@
-from .dualpath import DualData, DualData25, DualData28
+from .dualpath import DualData, DualData13, DualData25, DualData28
 from .singlepath import SingleData, SingleData13, SingleData25, SingleData28
