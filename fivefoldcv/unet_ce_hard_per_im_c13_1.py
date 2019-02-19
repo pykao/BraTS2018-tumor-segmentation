8f486884a7cc7ff7ef9441fa55e6eca0affff462
Po-Yu Kao <poyu_kao@ucsb.edu>
2019-02-18 20:53:26 -0800

    added seed 5713

--- fivefoldcv/unet_ce_hard_per_im_c13_1.py
+++ fivefoldcv/unet_ce_hard_per_im_c13_1.py
@@ -2,9 +2,7 @@ from subprocess import call
 
 model = 'unet_ce_hard_per_im_c13'
 
-#seeds = ['_s6916', '_s7435', '_s8841', '_s9527']
-
-seeds = ['_s5713']
+seeds = ['_s5713', '_s6916', '_s7435', '_s8841', '_s9527']
 
 for seed in seeds:
 	cfg_name = model+seed
