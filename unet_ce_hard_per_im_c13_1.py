d492fb869103cbc8c179bc8515ddd4ae79e9b66e
Po-Yu Kao <poyu_kao@ucsb.edu>
2019-02-27 14:15:27 -0800

    updated

---
+++ unet_ce_hard_per_im_c13_1.py
@@ -0,0 +1,9 @@
+from subprocess import call
+
+model = 'unet_ce_hard_per_im_c13'
+
+seeds = ['_s0423', '_s1420', '_s2859', '', '_s4867']
+
+for seed in seeds:
+	cfg_name = model+seed
+	call(['python', 'train_unet.py', '--gpu', '0', '--cfg', cfg_name])
